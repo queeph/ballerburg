b563526066cc9ff2fffdf987c5f1121e3d31fe24
queeph <67600793+queeph@users.noreply.github.com>
2025-09-20 20:45:15 +0000

    a

--- scripts/Projectile.gd
+++ scripts/Projectile.gd
@@ -7,7 +7,6 @@ signal exploded(position: Vector2, hit: Node)
 
 func _ready() -> void:
     body_entered.connect(_on_body_entered)
-    area_entered.connect(_on_area_entered)
 
 func launch(initial_velocity: Vector2, wind_accel: float) -> void:
     wind_acceleration = wind_accel
@@ -19,8 +18,4 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 
 func _on_body_entered(body: Node) -> void:
     emit_signal("exploded", global_position, body)
-    queue_free()
-
-func _on_area_entered(area: Area2D) -> void:
-    emit_signal("exploded", global_position, area)
     queue_free()
